b3e47e758b0a6a5899304f92f96588bc6c05f1a4
nishipy <goodisonev@gmail.com>
2020-09-18 18:09:05 +0900

    Add assembly for mult

--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -7,3 +7,42 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+//答えの初期化
+@2
+M=0
+
+//R0==0 || R1==0 ならば、@END にジャンプする
+@0
+D=M
+@END
+D;JEQ
+
+@1
+D=M
+@END
+D;JEQ
+
+(LOOP)
+    @2
+    D=M
+
+    @1
+    D=D+M
+
+    //RAM[2]を書き換える
+    @2
+    M=D
+
+    //RAM[0]の値を1減らす
+    @0
+    D=M-1
+    M=D
+
+    //RAM[0]>0である限りループする
+    @LOOP
+    D;JGT
+
+
+(END)
+    @END
+    0;JMP //これは、無限ループを表す
